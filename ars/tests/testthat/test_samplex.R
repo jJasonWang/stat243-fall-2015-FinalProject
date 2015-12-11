fdb9c3a536c6a56cc4a962f95db9928186c3c1a7
supermao10 <chaomao@berkeley.edu>
2015-12-11 15:21:59 -0800

    some change on test_samplex

--- ars/tests/testthat/test_samplex.R
+++ ars/tests/testthat/test_samplex.R
@@ -6,6 +6,6 @@ test_that("test the sampling function with some given u and y",{
   b <- c(5,6,20)
   u <- list(parameter=cbind(a,b),breaks=c(-Inf,0,9,Inf))
   actual <- samplex(u,c(0.1,0.5,0.7))
-  expect <- c(-0.02947048,  0.57736628,  1.08804536)
+  expect <- c(-0.02947047581504029165056,  0.57736627878524959811557, 1.08804535668570068196459)
   expect_equal(actual,expect)
 })
